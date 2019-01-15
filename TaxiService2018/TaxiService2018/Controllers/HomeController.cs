f83b7e418fc467fa6ae4d217536e69cfa7fab2e6
Bogdan Zugic <zugicbogdan@gmail.com>
2019-01-15 17:02:30 +0100

    Updates and improvements

--- TaxiService2018/TaxiService2018/Controllers/HomeController.cs
+++ TaxiService2018/TaxiService2018/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Web;
 using System.Web.Mvc;
 using TaxiService2018.Database;
@@ -42,11 +43,150 @@ namespace TaxiService2018.Controllers
                 rides = rides.Where(r => r.Driver.Id == user.Id);
             }
 
-            var rideTableRows = new List<RideTableSingleRow>();
-
+            //var rideTableRows = new List<RideTableSingleRow>();
+            var rideTableRows = GenerateQuery(rides).ToList().Select(r => new RideTableSingleRow(r));
 
             return View("Home", rideTableRows);
         }
+
+        [HttpGet]
+        public ActionResult Search()
+        {
+            var user = (ApplicationUser)Session["User"];
+            if (user == null)
+            {
+                return RedirectToAction("SignIn", "Login");
+            }
+
+            return View();
+        }
+        
+        #region query
+        private IQueryable<Ride> GenerateQuery(IQueryable<Ride> rides)
+        {
+            if (Request.QueryString["status"] != null)
+            {
+                var status = Request.QueryString["status"];
+
+                switch (status)
+                {
+                    case "1":
+                        rides = rides.Where(r => r.Status == RideStatus.Formed);
+                        break;
+                    case "2":
+                        rides = rides.Where(r => r.Status == RideStatus.Failed);
+                        break;
+                    case "3":
+                        rides = rides.Where(r => r.Status == RideStatus.Successful);
+                        break;
+                    case "0":
+                    default:
+                        break;
+                }
+            }
+
+            if (Request.QueryString["sortBy"] != null)
+            {
+                var sortBy = Request.QueryString["sortBy"];
+
+                switch (sortBy)
+                {
+                    case "1":
+                        rides = rides.OrderByDescending(r => r.OrderDateTime);
+                        break;
+                    case "2":
+                        rides = rides.OrderByDescending(r => r.Comment.Rating);
+                        break;
+                    case "0":
+                    default:
+                        break;
+                }
+            }
+
+            if (Request.QueryString["orderDateMin"] != null)
+            {
+                var orderDateMin = Request.QueryString["orderDateMin"];
+
+                if (DateTime.TryParse(orderDateMin, out DateTime result))
+                {
+                    rides = rides.Where(r => r.OrderDateTime >= result);
+                }
+            }
+
+            if (Request.QueryString["orderDateMax"] != null)
+            {
+                var orderDateMax = Request.QueryString["orderDateMax"];
+
+                if (DateTime.TryParse(orderDateMax, out DateTime result))
+                {
+                    rides = rides.Where(r => r.OrderDateTime <= result);
+                }
+            }
+
+            if (Request.QueryString["ratingMin"] != null)
+            {
+                var ratingMin = Request.QueryString["ratingMin"];
+
+                if (int.TryParse(ratingMin, out int result) && result > 0)
+                {
+                    rides = rides.Where(r => r.Status == RideStatus.Failed && (int)r.Comment.Rating >= result);
+                }
+            }
+
+            if (Request.QueryString["ratingMax"] != null)
+            {
+                var ratingMax = Request.QueryString["ratingMax"];
+
+                if (int.TryParse(ratingMax, out int result) && result > 0)
+                {
+                    rides = rides.Where(r => r.Status == RideStatus.Failed && (int)r.Comment.Rating <= result);
+                }
+            }
+
+            if (Request.QueryString["priceMin"] != null)
+            {
+                var priceMin = Request.QueryString["priceMin"];
+
+                if (int.TryParse(priceMin, out int result))
+                {
+                    rides = rides.Where(r => r.Status == RideStatus.Successful && r.Price.Value >= result);
+                }
+            }
+
+            if (Request.QueryString["priceMax"] != null)
+            {
+                var priceMax = Request.QueryString["priceMax"];
+
+                if (int.TryParse(priceMax, out int result))
+                {
+                    rides = rides.Where(r => r.Status == RideStatus.Successful && r.Price.Value <= result);
+                }
+            }
+
+            if (Request.QueryString["firstName"] != null)
+            {
+                var firstName = Request.QueryString["firstName"];
+
+                if (!string.IsNullOrWhiteSpace(firstName))
+                {
+                    rides = rides.Where(r => r.Driver.FirstName.ToLower().Contains(firstName.ToLower()));
+                }
+            }
+
+            if (Request.QueryString["lastName"] != null)
+            {
+                var lastName = Request.QueryString["lastName"];
+
+                if (!string.IsNullOrWhiteSpace(lastName))
+                {
+                    rides = rides.Where(r => r.Driver.LastName.ToLower().Contains(lastName.ToLower()));
+                }
+            }
+
+            return rides;
+        }
+        #endregion query
+        
 
         protected override void Dispose(bool disposing)
         {
